0034d7e32ff628d81732aad44ab9a33cb3c8520e
umarsamad <usamad@gmx.com>
2018-02-17 20:50:18 -0800

    Initial commit

---
+++ WinCCOPCXMLWebService/Library/OPC_XML_DA.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WinCCOPCXMLWebService.WinCCServer;
+
+
+namespace WinCCOPCXMLWebService.Library
+{
+    class OPC_XML_DA
+    {
+        /*---------------------------------------------Request Instance Decleration ----------------------------------------------------------------*/
+        OPCXML_DataAccess myClient = new OPCXML_DataAccess();
+        ReplyBase StateReply = new ReplyBase();
+        RequestOptions RequestOptions = new RequestOptions();
+        ServerStatus ReadStatus = new ServerStatus();
+
+        ReplyItemList ReplyList;
+        OPCError[] ErrorList;
+        bool ReturnValueOnReply;
+
+        public void SetConnection(string ServiceURL)
+        {
+            myClient.Url = ServiceURL;
+            //System.Net.ICredentials myCredentials = new
+            //System.Net.NetworkCredential("user", "password");
+            // myClient.Credentials = myCredentials;
+            // myClient.PreAuthenticate = true;
+            System.Net.ServicePointManager.Expect100Continue = false;
+        }
+
+
+        public bool OPCStatus()
+        {
+            /*---------------------------------------------Request Instance Decleration ----------------------------------------------------------------*/
+            string Status;
+            RequestOptions.ClientRequestHandle = "";
+            RequestOptions.LocaleID = "EN-US";
+            RequestOptions.RequestDeadlineSpecified = false;
+            RequestOptions.ReturnDiagnosticInfo = false;
+            RequestOptions.ReturnErrorText = false;
+            RequestOptions.ReturnItemName = false;
+            RequestOptions.ReturnItemPath = false;
+            RequestOptions.ReturnItemTime = false;
+
+            StateReply = myClient.GetStatus(RequestOptions.LocaleID, RequestOptions.ClientRequestHandle, out ReadStatus);
+
+            Status = StateReply.ServerState.ToString();
+
+            bool isOK = (Status == "running");
+
+            Console.WriteLine("\n" + ReadStatus.StatusInfo.ToString() + "\n");
+
+            return isOK;
+
+        }
+        /*---------------------------------------------Writing Function Decleration ----------------------------------------------------------------*/
+        public void OPCWrite(List<string> TagNames, List<dynamic> TagsValues)
+        {
+            /*---------------------------------------------Request Instance Decleration ----------------------------------------------------------------*/
+            WriteRequestItemList WriteItemList = new WriteRequestItemList();
+            List<string> TagName = new List<string>(TagNames);
+            List<dynamic> TagValue = new List<dynamic>(TagsValues);
+
+            RequestOptions.ClientRequestHandle = "";
+            RequestOptions.LocaleID = "EN-US";
+            RequestOptions.RequestDeadlineSpecified = false;
+            RequestOptions.ReturnDiagnosticInfo = false;
+            RequestOptions.ReturnErrorText = false;
+            RequestOptions.ReturnItemName = false;
+            RequestOptions.ReturnItemPath = false;
+            RequestOptions.ReturnItemTime = false;
+
+            int TagNos = TagName.Count();
+            WriteItemList.Items = new ItemValue[TagNos];
+            //Console.WriteLine(WriteItemList.Items.Length.ToString()); // Test for the No of Items
+
+            for (int Looper = 0; Looper < TagNos; Looper++)
+            {
+                WriteItemList.Items[Looper] = new ItemValue();
+                WriteItemList.ItemPath = "";
+                WriteItemList.Items[Looper].ItemName = TagName[Looper];
+                WriteItemList.Items[Looper].Value = TagValue[Looper];
+                Console.WriteLine(WriteItemList.Items[Looper].ItemName.ToString()+"Value Sent"+ WriteItemList.Items[Looper].Value.ToString()); // Test Output
+            }
+            ReturnValueOnReply = false;
+            /*---------------------------------------------Writing Invoked ----------------------------------------------------------------*/
+            myClient.Write(RequestOptions, WriteItemList, ReturnValueOnReply, out ReplyList, out ErrorList);
+            
+            TagNames = null;
+            TagValue = null;
+
+
+        }
+
+        /*---------------------------------------------Reading Function Decleration ----------------------------------------------------------------*/
+        public dynamic OPCRead(List<string> TagNames)
+        {
+
+/*---------------------------------------------Request Instance Decleration ----------------------------------------------------------------*/
+            List<string> TagName = new List<string>(TagNames);
+            List<dynamic> TagValue = new List<dynamic>();
+
+            int TagNos = TagName.Count();
+
+            ReadRequestItemList ReadItemList = new ReadRequestItemList();
+            ReadRequestItem[] ReadItemArray = new ReadRequestItem[TagNos];
+            ReadRequestItem ReadItem = new ReadRequestItem();
+            
+            RequestOptions.ClientRequestHandle = "";
+            RequestOptions.LocaleID = "EN-US";
+            RequestOptions.RequestDeadlineSpecified = false;
+            RequestOptions.ReturnDiagnosticInfo = false;
+            RequestOptions.ReturnErrorText = false;
+            RequestOptions.ReturnItemName = false;
+            RequestOptions.ReturnItemPath = false;
+            RequestOptions.ReturnItemTime = false;
+
+            for (int Looper = 0; Looper < TagNos; Looper++)
+            {
+                ReadItem.ItemPath = "";
+                ReadItem.ItemName = TagName[Looper];
+                ReadItemArray[Looper] = ReadItem;
+                ReadItemList.Items = ReadItemArray;
+            }
+
+/*---------------------------------------------Reading Function Invoked ----------------------------------------------------------------*/
+            myClient.Read(RequestOptions, ReadItemList, out ReplyList, out ErrorList);
+
+            if ((ReplyList.Items[0] != null) && (ReplyList.Items[0].Value != null) && (ReplyList.Items[0].Value.GetType().Name != "XmlNode[]"))
+            {
+
+/*---------------------------------------------Output to Console Invoked ----------------------------------------------------------------*/
+                for (int Looper = 0; Looper < TagNos; Looper++)
+                {
+                    TagValue.Add(ReplyList.Items[Looper].Value);
+                    //Console.WriteLine(ReplyList.Items[0].Value.ToString());
+                }
+                return TagValue; //ReplyList.Items[0].Value.ToString();
+            }
+            else
+            {
+                return "<Error>"; // Console.WriteLine("<Error>");//Output.Text = "<Error>";
+            }
+        
+        }
+        
+    }
+ }
+
+
+
---
+++ WinCCOPCXMLWebService/Program.cs
@@ -0,0 +1,83 @@
+﻿/*  
+    This Program has Been Written in order to have a Communicaiton to an XML DA based OPC Server.
+    The Server Side Used Here is SIMATIC WinCC Version 7.x with WinCC Connectivity Pack installed.
+    To Run the program make sure that the Server Side (Which ever is required) is installed and Properly configured. 
+    Also we have used Web Reference rather than the WCF's Service Reference as the WinCC OPC-XML-DA Services are based on .Net 2.x a.k.a Classic
+    
+    This Software is available to be freely used and published under the GPL v3 Licensing Terms. The Name of the Original Developer Should be retained as part 
+    of all of the documentation and any subsequent enhancements. 
+
+    Developer Name: Sheikh Umar Samad
+    Developer Web Address: http://www.umarsamad.com
+    Linked In profile: http://ae.linkedin.com/umarsamad
+    Email Address: usamad@gmx.com
+    
+    Limitations:
+    This Program is able to read and Write form/to WinCC using Open Connectivity Pack. 
+    The Below Code used Tag Names as "TagName1" "TagName2" "TagName3" "TagName4" "TagName5"
+    with Tag Values 1, 2, 3 ,4 and 5. 
+    In Order for this Code to Work these Tags should be declared at WinCC Side with Type Unsigned Int 16Bit 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+
+namespace WinCCOPCXMLWebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Library.OPC_XML_DA myInstance = new Library.OPC_XML_DA();
+            bool isOK;
+            string ServiceURL = "http://192.168.2.103/WinCC-OPC-XML/DAWebservice.asmx";
+            
+            List<string> TagNames = new List<string>();
+            List<dynamic> TagValues = new List<dynamic>();
+
+/*---------------------------------------------Server Connection  Decleration ----------------------------------------------------------------*/
+            myInstance.SetConnection(ServiceURL);
+
+            isOK = myInstance.OPCStatus();
+            if (isOK == false)
+            {
+                myInstance = null;
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                Environment.Exit(0);
+            }
+
+/*---------------------------------------------Writing Function Invoked ----------------------------------------------------------------*/
+            for (int Looper = 1; Looper <= 10; Looper++)
+            {
+                TagNames.Add(Convert.ToString("TagName"+Looper));// Pushing Tag Names to List. Generated Dynamic using Loop Index, However should be modified to fit application/real Tag Names Requirement
+                TagValues.Add(Looper); // Adding TagValue as the Loop index Values. However should be modified to fit application/real Tag Names Requirement
+            }                      
+            myInstance.OPCWrite(TagNames, TagValues);
+/*---------------------------------------------Delay for 4 Seconds ----------------------------------------------------------------*/
+            System.Threading.Thread.Sleep(4000); // Awaiting the Data to Be Written
+
+/*---------------------------------------------Reading Function Invoked ----------------------------------------------------------------*/
+            TagValues.Add(myInstance.OPCRead(TagNames));
+/*---------------------------------------------Delay for 4 Seconds ----------------------------------------------------------------*/
+            System.Threading.Thread.Sleep(4000); // Awaiting Data to Be populated
+
+            for (int Looper = 0; Looper < TagNames.Count; Looper++)
+            {
+                Console.WriteLine(TagNames[Looper]+" Value is: " + TagValues[Looper]); // Priting the Retrieved Tag Values to Screen
+            }
+
+/*---------------------------------------------Memory Cleanup ----------------------------------------------------------------*/
+            myInstance = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Environment.Exit(0);            
+        }
+    }
+}
